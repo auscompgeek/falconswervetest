67ffdd3f701497cff51a122d7a92f6026cd23313
OliverW10 <oliver@warrickfamily.com>
2021-02-10 21:03:37 +1100

    added steer pid, added new scalers and changed module state calculation

--- robot.py
+++ robot.py
@@ -6,7 +6,8 @@ from rev import CANSparkMax, MotorType, ControlType
 from wpimath.kinematics import SwerveDrive2Kinematics, ChassisSpeeds, SwerveModuleState
 from wpimath.geometry import Translation2d, Rotation2d
 
-from utilities.functions import constrain_angle, rescale_js
+from utilities.scalers import rescale_js, scale_value
+from utilities.functions import constrain_angle
 from wpimath.controller import SimpleMotorFeedforwardMeters
 
 
@@ -27,25 +28,31 @@ class SwerveModule:
         self.encoder.setPositionConversionFactor(math.tau/3.3)
         self.encoder.setInverted(True)
         self.hall_effect.setPositionConversionFactor(self.STEER_GEAR_RATIO * math.tau)
-        self.hall_effect.setPosition(self.encoder.getPosition())
+        self.rezero_hall_effect()
         self.steer_pid = steer.getPIDController()
         self.steer_pid.setFeedbackDevice(self.hall_effect)
+        self.steer_pid.setP(8.51)
+        self.steer_pid.setD(833.0)
+        self.steer_pid.setFF(0.583)
+        self.steer_pid.setSmartMotionMaxAccel(math.tau)
+        self.steer_pid.setSmartMotionMaxVelocity(math.tau)
 
         self.drive = drive
         self.drive_ff = SimpleMotorFeedforwardMeters(kS=0.757, kV=1.3, kA=0.0672)
 
-        drive.config_kP(slotIdx=0, value=1e-16, timeoutMs=20)
+        drive.config_kP(slotIdx=0, value=2.21e-31, timeoutMs=20)
     
-    def get_angle(self):
-        return self.hall_effect.getPosition()
+    def get_angle(self) -> Rotation2d:
+        return Rotation2d(self.hall_effect.getPosition())
 
     def get_speed(self):
         return self.drive.getSelectedSensorVelocity() * self.DRIVE_SENSOR_TO_METRES * 10
     
     def set(self, desired_state: SwerveModuleState):
-        self.steer_pid.setReference(desired_state.angle, ControlType.kSmartMotion)
+        self.steer_pid.setReference(desired_state.angle.radians(), ControlType.kPosition)
         # rescale the speed target based on how close we are to being correctly aligned
-        speed_target = desired_state.speed * math.cos(self.steer_pid.getLastError())
+        error = self.get_angle() - desired_state.angle
+        speed_target = desired_state.speed * error.cos()
         speed_volt = self.drive_ff.calculate(speed_target)
         voltage = wpilib.RobotController.getInputVoltage()
         self.drive.set(
@@ -63,19 +70,22 @@ class MyRobot(wpilib.TimedRobot):
     def robotInit(self):
         """Robot initialization function"""
         self.modules = [
-            SwerveModule(1, 1, CANSparkMax(4, MotorType.kBrushless), ctre.WPI_TalonFX(3)),
-            SwerveModule(-1, -1, CANSparkMax(6, MotorType.kBrushless), ctre.WPI_TalonFX(5))
+            SwerveModule(0.8/2-0.125, 0.75/2-0.1, CANSparkMax(9, MotorType.kBrushless), ctre.WPI_TalonFX(3)),
+            SwerveModule(-0.8/2-0.125, -0.75/2-0.1, CANSparkMax(7, MotorType.kBrushless), ctre.WPI_TalonFX(5))
         ]
 
         self.kinematics = SwerveDrive2Kinematics(self.modules[0].translation, self.modules[1].translation)
 
+        self.joystick = wpilib.Joystick(0)
+
+        self.spin_rate = 1.5
 
     def teleopInit(self):
         """Executed at the start of teleop mode"""
 
     def teleopPeriodic(self):
         """Runs the motors with tank steering"""
-        throttle = max(0.1, (1 - self.joystick.getThrottle()) / 2)  # min 10%
+        throttle = scale_value(self.joystick.getThrottle(), 1, -1, 0.1, 1)
 
         # this is where the joystick inputs get converted to numbers that are sent
         # to the chassis component. we rescale them using the rescale_js function,
@@ -83,25 +93,20 @@ class MyRobot(wpilib.TimedRobot):
         # which just means if it is under a certain value a 0 will be sent
         # TODO: Tune these constants for whatever robot they are on
         joystick_vx = -rescale_js(
-            self.joystick.getY(), deadzone=0.1, exponential=1.5, rate=4 * throttle
-        )
+            self.joystick.getY(), deadzone=0.1, exponential=1.5
+        ) * 4 * throttle
         joystick_vy = -rescale_js(
-            self.joystick.getX(), deadzone=0.1, exponential=1.5, rate=4 * throttle
-        )
+            self.joystick.getX(), deadzone=0.1, exponential=1.5
+        ) * 4 * throttle
         joystick_vz = -rescale_js(
-            self.joystick.getZ(), deadzone=0.2, exponential=20.0, rate=self.spin_rate
-        )
-        if joystick_vx or joystick_vy or joystick_vz:
-            chassis_speeds = ChassisSpeeds(joystick_vx, joystick_vy, joystick_vz)
-        else:
-            chassis_speeds = ChassisSpeeds(0, 0, 0)
+            self.joystick.getZ(), deadzone=0.2, exponential=20.0
+        ) * self.spin_rate
+        chassis_speeds = ChassisSpeeds(joystick_vx, joystick_vy, joystick_vz)
 
-        module_states = self.kinematics.toSwerveModuleStates(chassis_speeds)
-        for i, module_state in enumerate(module_states):
-            angle = Rotation2d(self.modules[i].get_angle())
-            module_states[i] = SwerveModuleState.optimize(module_state, angle)
-
-
+        
+        for state, module in zip(self.kinematics.toSwerveModuleStates(chassis_speeds), self.modules):
+            state = SwerveModuleState.optimize(state, module.get_angle())
+            module.set(state)
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
